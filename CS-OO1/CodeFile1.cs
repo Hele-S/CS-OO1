d0cedf884b29c51e34e7b756e5507b42706786dd
helenaldo.silva <helenaldo.silva@SUPORTE52>
2026-06-15 15:59:42 -0300

    Adicionar arquivos de projeto.

---
+++ CS-OO1/CodeFile1.cs
@@ -0,0 +1,11 @@
+﻿class EscolaDoRock
+{
+    public string titulo;
+    public int anoLancamento;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Título: {titulo}");
+        Console.WriteLine($"Ano de Lançamento: {anoLancamento}");
+    }
+}
---
+++ CS-OO1/Program.cs
@@ -0,0 +1,8 @@
+﻿Musica teste = new Musica();
+
+teste.nome = "Bohemian Rhapsody";
+teste.artista = "Queen";
+teste.duracao = 354;
+teste.disponivel = true;
+
+teste.toString();
